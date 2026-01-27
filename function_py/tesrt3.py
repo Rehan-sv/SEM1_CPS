4e372fca36285c9cc597a49990a6fb66663fa38d
REHAN SV <rehansv1@gmail.com>
2026-01-27 22:25:53 +0530

    Add book return functionality to the book management system

---
+++ function_py/tesrt3.py
@@ -0,0 +1,9 @@
+def r(l):
+    t=input("enter the book to return:")
+    if t in l:
+        if l[t]['available']:
+            available=True
+            print("the book returned successfully")
+        else:
+            print("the book is already returned")
+    else:
